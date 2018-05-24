e65a245150b01f85ce5c5ff2c697ef4159f0e253
eric-yuanxufeng <31099708+eric-yuanxufeng@users.noreply.github.com>
2018-05-24 11:26:13 +1000

    Add files via upload

---
+++ IOS ASSIGNMENT 3/IOS ASSIGNMENT 3Tests/IOS_ASSIGNMENT_3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  IOS_ASSIGNMENT_3Tests.swift
+//  IOS ASSIGNMENT 3Tests
+//
+//  Created by Xufeng Yuan on 24/5/18.
+//  Copyright © 2018 Xufeng Yuan. All rights reserved.
+//
+
+import XCTest
+@testable import IOS_ASSIGNMENT_3
+
+class IOS_ASSIGNMENT_3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
