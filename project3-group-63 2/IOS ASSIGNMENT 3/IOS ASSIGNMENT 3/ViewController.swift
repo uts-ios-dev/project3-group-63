b723c4106875c5553fbae2e61b6e0128d2c2ec45
eric-yuanxufeng <31099708+eric-yuanxufeng@users.noreply.github.com>
2018-06-07 13:00:42 +1000

    Add files via upload

---
+++ project3-group-63 2/IOS ASSIGNMENT 3/IOS ASSIGNMENT 3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOS ASSIGNMENT 3
+//
+//  Created by Xufeng Yuan on 24/5/18.
+//  Copyright © 2018 Xufeng Yuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
